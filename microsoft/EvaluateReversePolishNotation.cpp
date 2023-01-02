32673aab343514e330194ee42cde72268246e06e
Sakshi Bhatia <40262132+sakshi79@users.noreply.github.com>
2023-01-02 10:15:14 +0530

    Create EvaluateReversePolishNotation.cpp

---
+++ microsoft/EvaluateReversePolishNotation.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack <int> st;
+        for(auto x: tokens)
+        {
+            if(x=="+")
+            {
+                int op1 = st.top();
+                st.pop();
+                int op2 = st.top();
+                st.pop();
+                st.push(op1+op2);
+            }
+            else if(x=="-")
+            {
+                int op1 = st.top();
+                st.pop();
+                int op2 = st.top();
+                st.pop();
+                st.push(op2-op1);
+            }
+            else if(x=="*")
+            {
+                int op1 = st.top();
+                st.pop();
+                int op2 = st.top();
+                st.pop();
+                st.push(op1*op2);
+            }
+            else if(x=="/")
+            {
+                int op1 = st.top();
+                st.pop();
+                int op2 = st.top();
+                st.pop();
+                st.push(op2/op1);
+            }
+            else
+                st.push(stoi(x));
+        }
+        return st.top();
+    }
+};
