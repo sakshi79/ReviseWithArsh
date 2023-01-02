46333f6aeaf3382c919d19b168a39b098f010981
Sakshi Bhatia <40262132+sakshi79@users.noreply.github.com>
2023-01-02 10:54:07 +0530

    Create CombinationSumIII.cpp

---
+++ microsoft/CombinationSumIII.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    vector <vector <int> > res;
+    void try_combs(vector <int> &combination, int start, int k, int n)
+    {
+        if(k==combination.size() && n==0)
+        {
+            res.push_back(combination);
+            return;
+        }
+        
+        for(int i=start; i<10; i++)
+        {
+            combination.push_back(i);
+            try_combs(combination, i+1, k, n-i);
+            combination.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector <int> combination;
+        try_combs(combination, 1, k, n);
+        return res;
+    }
+};
