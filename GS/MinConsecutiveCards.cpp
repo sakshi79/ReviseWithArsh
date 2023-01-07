5e3d73df5b5db952c16fd072fe80917d62c65282
Sakshi Bhatia <40262132+sakshi79@users.noreply.github.com>
2023-01-07 21:12:29 +0530

    Create MinConsecutiveCards.cpp

---
+++ GS/MinConsecutiveCards.cpp
@@ -0,0 +1,24 @@
+// Question link: 
+
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& cards) {
+        int res = INT_MAX;
+        int n = cards.size();
+        unordered_map <int, int> m;
+        for(int i=0; i<n; i++)
+        {
+        
+            if(m.find(cards[i]) != m.end())
+            {
+                
+                int curr = abs(m[cards[i]] - i) + 1;
+                res = min(res, curr);
+            }
+            m[cards[i]] = i;
+        }
+        if(res == INT_MAX)
+            return -1;
+        return res;
+    }
+};
