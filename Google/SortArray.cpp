3c57d6eda721a2a06f81cc5a1fc97b7dd167a51c
Sakshi Bhatia <40262132+sakshi79@users.noreply.github.com>
2023-01-22 21:07:31 +0530

    Create SortArray.cpp

---
+++ Google/SortArray.cpp
@@ -0,0 +1,31 @@
+// Question link: https://leetcode.com/problems/sort-an-array/
+
+class Solution {
+    void heapify(vector<int>& arr, int n, int i)
+    {
+        int largest = i;
+        int left = 2*i+1;
+        int right = 2*i+2;
+        if(left < n && arr[left]>arr[largest])
+            largest = left;
+        if(right < n && arr[right]>arr[largest])
+            largest = right;
+        if(largest != i)
+        {
+            swap(arr[i], arr[largest]);
+            heapify(arr, n, largest);
+        }
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = n/2 -1; i>=0; i--)
+            heapify(nums, n, i);
+        for(int i = n-1; i>=1; i--)
+        {
+            swap(nums[0], nums[i]);
+            heapify(nums, i, 0);
+        }
+        return nums;
+    }
+};
